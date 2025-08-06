91b1b87b6cce9b964c208354b2928ebd82269558
YeeunSeok <yes3641@naver.com>
2025-08-06 10:09:39 +0900

    Initial commit

---
+++ src/main/java/ac/su/kdt/bepaymentservice/BePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.bepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/kdt/bepaymentservice/BePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.bepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
